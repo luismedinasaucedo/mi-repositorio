857becd43cad7c3dfddd2d444bc47c492a4f2fcc
luismedinasaucedo <medinasaucedoluiscarlo@gmail.com>
2025-09-30 10:58:36 -0600

    archivo colas(Funciona)

---
+++ Java/EdT3A4-Colas/src/main/java/com/mycompany/edt3a4/colas/Cola.java
@@ -0,0 +1,58 @@
+package com.mycompany.edt3a4.colas;
+
+public class Cola {
+private String ColaInicial[];
+private int Max,inicio,fin,numcola;
+
+    public Cola(int Max) {
+        this.Max = Max;
+        this.ColaInicial = new String[Max];
+        this.inicio = 0;
+        this.fin = 0;
+        this.numcola = 0;
+    }
+    public void Encolar(String d){
+        if (ColaLlena()) {
+            System.out.println("no se puede agregar dato: cola llena");
+        }
+        else {
+            ColaInicial[fin]=d;
+            fin=(fin+1)%Max;
+        }
+    }
+    
+    public String Desencolar(){
+        if (ColaVacia()) {
+            System.out.println("no se puede eliminar dato: Cola vacia");
+            return null; 
+        }else {
+        String valor = ColaInicial[inicio];
+        inicio = (inicio + 1) % Max; 
+        return valor;
+        }
+        
+        
+    }
+    public boolean ColaVacia(){
+        return inicio==fin;
+    }
+    public boolean ColaLlena(){
+    return (fin+1)%Max==inicio;
+    
+    }
+    public void mostrar(){
+        if (ColaVacia()) {
+            System.out.println("la cola esta vacia");
+            return;
+        }else {
+        int i= inicio;
+            System.out.println("cola: ");
+            while(i!=fin){
+            
+            }
+        }
+            
+        }
+    }
+    
+
---
+++ Java/EdT3A4-Colas/src/main/java/com/mycompany/edt3a4/colas/EdT3A4Colas.java
@@ -0,0 +1,35 @@
+package com.mycompany.edt3a4.colas;
+
+import java.util.Scanner;
+
+public class EdT3A4Colas {
+
+    public static void main(String[] args) {
+        Scanner leer=new Scanner(System.in);
+        int ops=0;
+        System.out.println("escribe el tamaño de la cola");
+        Cola nombres=new Cola(leer.nextInt());
+        do {
+            System.out.println("1.- añadir dato");
+            System.out.println("2.- eliminar dato");
+            System.out.println("3.- salir");
+            ops=leer.nextInt();
+            switch(ops){
+            case 1->{
+                System.out.println("ingresa el nombre");
+                String d=leer.next();
+                nombres.Encolar(d);
+            }
+            case 2->{
+                nombres.Desencolar();
+            }
+            case 3->{System.out.println("eligio salir");}
+            
+        
+        }
+        } while (ops!=3);
+        
+        
+        
+    }
+}
