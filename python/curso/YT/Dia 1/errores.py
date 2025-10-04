479d43c1ed23d36d5071b09de5367a4465a5775e
Luis Medina <medinasaucedoluiscarlo@gmail.com>
2025-10-04 12:48:39 -0600

    dia 2 curso

---
+++ python/curso/YT/Dia 1/errores.py
@@ -0,0 +1,9 @@
+butterflies = 10
+beetles = 12
+ladybugs = 20
+total=butterflies+beetles+ladybugs
+print('I caught ' + str(butterflies) + ' 🦋 butterflies!')
+print('I caught ' + str(beetles) + ' 🪲 beetles!')
+print('I caught ',ladybugs ,' ladybugs!')#es otra forma de añador datos que no son str
+print('I caught ' + str(total) + ' total bugs!')
+print(4!=5)
---
+++ python/curso/YT/Dia 2/area.py
@@ -0,0 +1,22 @@
+pi=3.14
+rad=5
+area=pi*rad**2
+print(area)
+
+oracion="I hope this course is not full of jargon"
+if "jargon" in oracion:
+    print("si")
+else:
+    print("no")
+
+num=7
+if num%2==0:
+    print("es primo")
+else:
+    print("no es primo")
+
+if 7//3==int(2.7):
+    print("es verdad")
+
+for i in range(1,6):
+    print(i,1,i,i**2,i**3)
