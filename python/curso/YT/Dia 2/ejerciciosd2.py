f417ed3f9ba89c3866f548b98d97a82ffaa963b2
Luis Medina <medinasaucedoluiscarlo@gmail.com>
2025-10-05 13:24:30 -0600

    ejercicios dia 2

---
+++ python/curso/YT/Dia 2/ejerciciosd2.py
@@ -0,0 +1,22 @@
+lista=["luis","carlo","medina"]
+print(lista[0])
+print(lista[len(lista)-1])
+print(lista[len(lista)//2])
+lista.insert(3,"saucedo")
+print(lista)
+lista[1]=lista[1].upper()
+print(lista[1])
+"#; lista"
+print(lista.__contains__("luis"))
+print("luis" in lista)
+print(lista[1:4])
+del lista[0]
+print(lista)
+
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node','Express', 'MongoDB']
+full_stack= front_end+back_end
+#full_stack.insert(full_stack.index('Redux')+1,"python")
+
+full_stack[full_stack.index('Redux'):full_stack.index('Redux')] = ['Python', 'SQL']
+print(full_stack)
