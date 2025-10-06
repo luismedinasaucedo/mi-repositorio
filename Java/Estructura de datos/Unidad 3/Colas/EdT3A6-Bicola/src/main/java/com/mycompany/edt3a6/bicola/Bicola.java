3c5038390dce4f8560581684b2e8b147e09b7ef9
Luis Medina <medinasaucedoluiscarlo@gmail.com>
2025-10-06 17:55:44 -0600

    Bicola agregar al frente y al final + main

--- Java/Estructura de datos/Unidad 3/Colas/EdT3A6-Bicola/src/main/java/com/mycompany/edt3a6/bicola/Bicola.java
+++ Java/Estructura de datos/Unidad 3/Colas/EdT3A6-Bicola/src/main/java/com/mycompany/edt3a6/bicola/Bicola.java
@@ -11,15 +11,43 @@ public class Bicola {
     public void insertaralfrente(String d){
         Nodo Cola=new Nodo(d);
         if (colavacia()) {
+            Inicio=Fin=Cola;
+        }
+        else{
+            Inicio.setAnt(Cola);
+            Cola.setSig(Inicio);
             Inicio=Cola;
+            
         }
         
     }
-    public void insertaralfinal(){}
+    public void insertaralfinal(String d){
+        Nodo Cola=new Nodo(d);
+        if (colavacia()) {
+            Inicio=Fin=Cola;
+        }
+        else{
+            Fin.setSig(Cola);
+            Cola.setAnt(Fin);
+            Fin=Cola;
+        }
+        
+        
+        
+    }
     public String eliminaralfrente(){return null;}
     public String eliminaralfin(){return null;}
     public boolean colavacia(){
         return Inicio==null;
+    }
+    public void mostrar(){
+        int i=1;
+    Nodo Aux=Inicio;
+    while(Aux!=null){
+        System.out.println(i+".-"+Aux.getDato());
+        Aux=Aux.getSig();
+        i++;
+    }
     }
     
     
--- Java/Estructura de datos/Unidad 3/Colas/EdT3A6-Bicola/src/main/java/com/mycompany/edt3a6/bicola/EdT3A6Bicola.java
+++ Java/Estructura de datos/Unidad 3/Colas/EdT3A6-Bicola/src/main/java/com/mycompany/edt3a6/bicola/EdT3A6Bicola.java
@@ -1,8 +1,32 @@
 package com.mycompany.edt3a6.bicola;
 
+import java.util.Scanner;
+
 public class EdT3A6Bicola {
 
     public static void main(String[] args) {
+        Scanner leer=new Scanner(System.in);
+        Bicola cola=new Bicola();
+        int ops=0;
+        while(ops!=6){
+            System.out.println("\tMenu");
+            System.out.println("1.- añadir dato al principio \n2.- añadir dato al final \n3.- eliminar dato al principio \n4.- eliminar dato al final \n5.- Mostrar datos\n6.- salir");
+            ops=leer.nextInt();
+            switch(ops){
+                case 1->{
+                    System.out.println("escribe al dato a insertar");
+                    cola.insertaralfrente(leer.next());
+                    }
+                case 2->{
+                    System.out.println("escribe el dato a insertar");
+                    cola.insertaralfinal(leer.next());
+                }
+                case 3->{}
+                case 4->{}
+                case 5->{cola.mostrar();}
+                case 6->{System.out.println("Eligio salir...");}
+            }
+        }
         
     }
 }
