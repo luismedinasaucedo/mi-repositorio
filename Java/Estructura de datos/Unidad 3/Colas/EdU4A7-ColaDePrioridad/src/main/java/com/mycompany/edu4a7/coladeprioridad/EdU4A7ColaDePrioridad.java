b0abef8f22fe168adc5e6381777acd046c543d7c
luismedinasaucedo <medinasaucedoluiscarlo@gmail.com>
2025-10-08 11:38:33 -0600

    puta lechuga

--- Java/Estructura de datos/Unidad 3/Colas/EdU4A7-ColaDePrioridad/src/main/java/com/mycompany/edu4a7/coladeprioridad/EdU4A7ColaDePrioridad.java
+++ Java/Estructura de datos/Unidad 3/Colas/EdU4A7-ColaDePrioridad/src/main/java/com/mycompany/edu4a7/coladeprioridad/EdU4A7ColaDePrioridad.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.edu4a7.coladeprioridad;
 
+import java.util.Scanner;
+
 /**
  *
  * @author luis
@@ -11,6 +13,30 @@ package com.mycompany.edu4a7.coladeprioridad;
 public class EdU4A7ColaDePrioridad {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner leer=new Scanner(System.in);
+        int ops=0;
+        System.out.println(" introduce max de datos: ");
+        Cola nombres=new Cola();
+        do {
+            System.out.println("menu");
+            System.out.println("1.- agregar");
+            System.out.println("2.- eliminar");
+            System.out.println("3.- mostrar");
+            System.out.println("4.- salir");
+            ops=leer.nextInt();
+            switch(ops){
+                case 1->{
+                    System.out.println("introduce nombre: ");
+                    String i=leer.next();
+                    System.out.println("introduce la prioridad: ");
+                    int e=leer.nextInt();
+                    nombres.encolar(i,e);}
+                case 2->{nombres.desencolar();}
+                case 3->{nombres.mostrar();}
+            
+            
+            }
+        } while (ops!=4);
+        System.out.println("");
     }
 }
