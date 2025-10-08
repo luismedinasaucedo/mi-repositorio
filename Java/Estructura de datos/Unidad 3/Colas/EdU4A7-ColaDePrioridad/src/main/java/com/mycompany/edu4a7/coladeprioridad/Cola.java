43d23d4124c47ce1a12fe096ccf67c6e4cc5a40c
luismedinasaucedo <medinasaucedoluiscarlo@gmail.com>
2025-10-08 11:53:55 -0600

    tu mama

--- Java/Estructura de datos/Unidad 3/Colas/EdU4A7-ColaDePrioridad/src/main/java/com/mycompany/edu4a7/coladeprioridad/Cola.java
+++ Java/Estructura de datos/Unidad 3/Colas/EdU4A7-ColaDePrioridad/src/main/java/com/mycompany/edu4a7/coladeprioridad/Cola.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.edu4a7.coladeprioridad;
-
-/**
- *
- * @author luis
- */
 public class Cola {
+    private Proceso inicio,fin;
+
+    public Cola() {
+        this.inicio = null;
+        this.fin = null;
+    }
+    public void encolar(String d,int e){
+    Proceso nuevo =new Proceso(d,e);
+        if (inicio==null) {
+            inicio=fin=nuevo;
+        }
+        else{
+        fin.setSig(nuevo);
+        }
+        fin=nuevo;
+    }
+    public String desencolar(){
+        if (inicio==null) {
+            return null;
+        }
+    String s=inicio.getDato();
+    inicio=inicio.getSig();
+        if (inicio==null) {
+            fin=null;
+        }
+    return s;
+    }
+    public void mostrar(){
+    Proceso Aux=inicio;
+    while(Aux!=null){
+        System.out.println(Aux.getDato());
+        Aux=Aux.getSig();
+    }
+    }
+    public boolean ColaVacia(){
+        return inicio==null;
+    }
+    
     
 }
--- python/Estructura de datos/EDT3A6-ColasDinamicas.py
+++ python/Estructura de datos/EDT3A6-ColasDinamicas.py
@@ -1,3 +1,52 @@
 class Nodo:
     def __init__(self,Dato):
-        self.Dato=
+        self.Dato=Dato
+        self.Sig=None
+    
+
+class Cola:
+    def __init__(self):
+        self.fin=None
+        self.inicio=None
+
+    def encolar(self,d):
+        Nodo1 =Nodo(d)
+        if self.inicio==None:
+            self.inicio=self.fin=Nodo1
+        else:
+            self.fin.Sig=Nodo1
+
+    def ColaVacia(self):
+        return self.inicio==None
+    
+    def mostrar(self):
+        if self.ColaVacia():
+            print("La cola esta vacia")
+        Aux=self.inicio
+        while(not Aux==None):
+            print(Aux.Dato)
+            Aux=Aux.Sig
+    def desencolar(self):
+        if self.ColaVacia():
+            print("La cola esta vacia")
+            return None
+        s=self.inicio.Dato
+        self.inicio=self.inicio.Sig
+        return s
+        
+        
+cola1=Cola()
+ops=0
+while not ops==4:
+    print("1.- añadir")
+    print("2.- eliminar")
+    print("3.- mostrar")
+    print("4.- salir")
+    ops=int(input("elige la opcion: "))
+    match ops:
+        case 1:
+            cola1.encolar(input("escribe el dato a agregar: "))
+        case 2:
+            cola1.desencolar()
+        case 3:
+            cola1.mostrar()
